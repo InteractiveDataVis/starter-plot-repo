9bdcbf4452f532f2cca0624ea471b17506db9af2
Ellie Frymire <efrymire@netflix.com>
2025-12-14 13:47:14 -0800

    simple initial example

---
+++ main.js
@@ -0,0 +1,8 @@
+// Importing css file here (instead of a link in index.html) because of the vite dev server / bundler
+import './main.css';
+
+import './components/plot_example.js';
+import './components/d3_example.js';
+
+console.log('main.js loaded');
+
