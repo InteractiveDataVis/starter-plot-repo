9bca5ee5dc368afd8b11e29931cb33aacf2e5440
Ellie Frymire <efrymire@netflix.com>
2025-12-15 13:34:32 -0800

    add other examples

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   base: '/starter-plot-repo/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        interactive: resolve(__dirname, 'interactive/index.html'),
+        growing: resolve(__dirname, 'growing/index.html'),
+        snowing: resolve(__dirname, 'snowing/index.html'),
+      },
+    },
+  },
 })
 
